8168cae31997ab9be6c937d96c6ae378653dc5e0
Andre Tan <tanandre93@gmail.com>
2016-07-14 16:40:53 +0800

    Send emails using mailchimp

--- MailChimpHandler.py
+++ MailChimpHandler.py
@@ -1,12 +1,14 @@
 import requests
 import hashlib
 from config import mailchimpkey
+from pprint import pprint
 
 class MailChimpHandler:
     def __init__(self):
         self.url = 'https://us8.api.mailchimp.com/3.0'
         self.auth = ('andre', mailchimpkey)
         self.agentListId = '435b3eca36'
+        self.campaignId = '1a410c9956' # Training Confirmation - DO NOT DELETE
         self.logs = {
             "Updated Profiles" : [],
             'New Subscriptions' : [],
@@ -39,9 +41,42 @@ class MailChimpHandler:
             self.logs['New Subscriptions'].append(item['name'] + " subscribed.")
         else:
             self.logs["Errors"].append("Invalid http request: " + item['name'])
+        return r.json()
 
-    def sendEmail(self, member_id):
-        return None
+    def sendEmails(self, allEmails, settings, tracking):
+        # Replicate previous campaign
+        replica = requests.post(self.url + '/campaigns/' + self.campaignId + '/actions/replicate', auth = self.auth).json()
+        newId = replica['id']
+
+        # Create new segment
+        segmentBody = {
+            'name': settings['title'],
+            'static_segment': allEmails
+        }
+        newSegment = requests.post(self.url + '/lists/' + self.agentListId + '/segments', auth = self.auth, json = segmentBody).json()
+
+        # Update campaign details
+        campaignBody = {
+            'type': 'regular',
+            'recipients': {
+                'list_id': self.agentListId,
+                'segment_opts': {
+                    'saved_segment_id': newSegment['id']
+                }
+            },
+            'settings': settings,
+            'tracking': tracking
+        }
+        updateCampaign = requests.patch(self.url + '/campaigns/' + newId, json = campaignBody, auth = self.auth).json()
+
+        # Send email
+        body = {
+            'test_emails': ['sihaoo93@gmail.com', 'tanandre93@gmail.com'],
+            'send_type': "html"
+        }
+        sendEmail = requests.post(self.url + '/campaigns/' + newId + '/actions/send', auth = self.auth, json = body).json()
+        return sendEmail.status_code == '204'
+
 
     def printLogs(self):
         for log in self.logs:
