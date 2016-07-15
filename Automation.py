4335f8446f229f505f99e8a65b2ddcd7c6677663
Andre Tan <tanandre93@gmail.com>
2016-07-15 15:50:12 +0800

    Add scheduler

--- Automation.py
+++ Automation.py
@@ -4,17 +4,22 @@ from MailChimpHandler import MailChimpHandler
 from SentlyHandler import SentlyHandler
 import sys, json
 from datetime import datetime
+from Logger import log
+import schedule, time
 
 class Automation:
-    def __init__(self):
-        isSafe = raw_input("Welcome! Type 'unsafe' to run without warnings. Enter any other key to proceed in safe mode.\n>> ")
-        self.safeMode = False if isSafe.lower().strip() == 'unsafe' else True
+    def __init__(self, safe = False):
+        self.safeMode = safe
+        msg = "START - safe mode ON" if self.safeMode else "START - safe mode OFF"
+        log(msg)
 
     def run(self):
+        print "Running schedule: " + datetime.now().strftime("%H:%M:%S %d %B")
         gsh = GSpreadHandler()
         msg = "Press [Enter] to update the list of unsent emails and SMS.\nType 'Exit' to quit the program.\n>> "
         self.checkExit(msg)
         gsh.updateFromLatest()
+        log('List of unsent emails and SMS has been updated.')
 
         unsent_sms = json.loads(open("unsent_sms.json").read())
         unsent_emails = json.loads(open("unsent_emails.json").read())
@@ -49,10 +54,13 @@ class Automation:
 
         msg = "Press [Enter] to send out emails to all newly subscribed users.\nType 'Exit' to quit the program.\n>> "
         self.checkExit(msg)
-        mch.sendEmails(allEmails, settings, tracking)
+        mch.sendEmails(allEmails, settings, tracking) # TODO: check status code when this succeeds
+        log("Emails sent to " + ", ".join(allEmails))
         gsh.emailSent(allTimestamps)
-        mch.printLogs()
+        log("Google Sheets updated with sent emails")
 
+        msg = "Press [Enter] to proceed with sending reminder SMS.\nType 'Exit' to quit the program.\n>> "
+        self.checkExit(msg)
         allTimestamps = []
         for item in unsent_sms:
             timestamp = slh.sendSms(item)
@@ -65,9 +73,10 @@ class Automation:
             curr += 1
             if curr == 10:
                 print "Number of tries exceeded"
+                log("Exceeded maximum number of tries to check if SMS is sent.")
                 sys.exit(0)
         gsh.smsSent(allTimestamps)
-
+        log("Google Sheets updated with sent SMS\n\n#####\n")
 
     def checkExit(self, msg):
         if self.safeMode:
@@ -79,4 +88,8 @@ class Automation:
 
 if __name__=="__main__":
     app = Automation()
-    app.run()
+    schedule.every(3).hours.do(app.run)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(900)
